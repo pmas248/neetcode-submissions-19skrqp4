b6a4300db02039b52277da86d286f75ea124c809
pmas248 <purveshrm@gmail.com>
2026-04-18 11:46:11 +1200

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        '''res = []
+        for i in range( len(temperatures)-1):
+            curr_temp = temperatures[i]
+            fut_temp = temperatures[i+1]
+            count, j = 0, i+1
+            while curr_temp >= temperatures[j] and j<len(temperatures)-1:
+                print(curr_temp, temperatures[j])
+                count += 1
+                j = j+1 
+            if curr_temp < temperatures[j]:
+                count += 1
+                res.append(count)
+            else:
+                res.append(0)
+
+        if temperatures != []:
+            res.append(0)
+        return res'''
+
+        res = [0] * len(temperatures)
+        stack = [] #stores tuples (ind, val)
+
+        for i, v in enumerate(temperatures):
+            while stack and v > stack[-1][1]:
+                stackI, stackV = stack.pop()
+                res[stackI] = i - stackI
+            stack.append((i, v))
+        return res
