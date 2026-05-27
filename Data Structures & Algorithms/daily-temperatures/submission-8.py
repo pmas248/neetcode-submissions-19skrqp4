f52fe278f3a9df5c4afa505acf2c0b28cbae9bbc
pmas248 <purveshrm@gmail.com>
2026-05-27 22:33:02 +1200

    Add: daily-temperatures - submission-8

---
+++ Data Structures & Algorithms/daily-temperatures/submission-8.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures) #In case no hotter day exists, default=0
+        temp = [] #stores tuples (index, temp)
+
+        for i, v in enumerate(temperatures):
+            while temp and v > temp[-1][1]: #if stack is empty and if curr > head
+                dayI, tmpV = temp.pop()
+                res[dayI] = i - dayI
+            temp.append((i, v)) 
+        return res
